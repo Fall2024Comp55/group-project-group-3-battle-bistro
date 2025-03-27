a9fd7e37b7bfeaa102aed9061a3136166519d0d7
4falcon4 <falcon@4falcon4.com>
2025-03-26 23:15:11 -0700

    Introduce possible enum-based structure for tower upgrades and attack behavior in MeleeTower class

--- src/Tower/MeleeTower.java
+++ src/Tower/MeleeTower.java
@@ -6,6 +6,7 @@ import Utils.TickListener;
 import acm.graphics.GObject;
 
 public class MeleeTower extends Tower implements TickListener {
+    // Might also be able to use enum for updgrade code
     // private Action variable that can store code
     private Action attack1;
 
@@ -57,6 +58,39 @@ public class MeleeTower extends Tower implements TickListener {
 
     @Override
     public void onCollision() {
+
+    }
+
+    // I am interested in using enums for this. If you want to try it out we can work on it together
+    private enum TowerVersions {
+        BASE {
+            @Override
+            void attack() {
+
+            }
+        },
+        UPGRADE1 {
+            @Override
+            void attack() {
+
+            }
+        },
+        UPGRADE2 {
+            @Override
+            void attack() {
+
+            }
+        },
+        UPGRADE3 {
+            @Override
+            void attack() {
+
+            }
+        };
+
+        abstract void attack();
+
+        // other version specific code
 
     }
 
