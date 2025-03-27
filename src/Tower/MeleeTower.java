df9874934d8e3e62ba843efa252536715868e1da
4falcon4 <falcon@4falcon4.com>
2025-03-26 22:20:19 -0700

    Implement attack actions in MeleeTower class and enhance upgrade method for dynamic attack behavior

--- src/Tower/MeleeTower.java
+++ src/Tower/MeleeTower.java
@@ -1,10 +1,13 @@
 package Tower;
 
+import Utils.Action;
 import Utils.GameTick;
 import Utils.TickListener;
 import acm.graphics.GObject;
 
 public class MeleeTower extends Tower implements TickListener {
+    // private Action variable that can store code
+    private Action attack1;
 
     public MeleeTower() {
         super("chefkirby", 1, 1, 1, 50);
@@ -12,11 +15,23 @@ public class MeleeTower extends Tower implements TickListener {
 
     @Override
     public void attack() {
-        //No
+        // preform attack code
+        attack1.performAction();
     }
 
     @Override
     public void upgrade() {
+        // setting attack1 to new attack code
+        if (level == 1) {
+            level = 2;
+            attack1 = () -> {
+                System.out.println("Attack 2");
+            };
+        } else if (level == 2) {
+            attack1 = () -> {
+                System.out.println("Attack 3");
+            };
+        }
         System.out.println("Upgrading");
     }
 
