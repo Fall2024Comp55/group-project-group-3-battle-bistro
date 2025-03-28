1ba0fa4f2522a0bf7b4b827e8dff47582f7c332d
4Falcon4 <falcon@4falcon4.com>
2025-03-28 16:07:57 -0700

    Add button class with label and box initialization

--- src/UI/Button.java
+++ src/UI/Button.java
@@ -2,15 +2,32 @@ package UI;
 
 import Utils.MouseInteract;
 import acm.graphics.GCompound;
+import acm.graphics.GImage;
 import acm.graphics.GLabel;
-import acm.graphics.GObject;
+import acm.graphics.GRect;
 
 import java.awt.*;
 import java.awt.event.MouseEvent;
 
 public class Button extends GCompound implements MouseInteract {
+    private static final String globalFont = "Arial-20";
+    private static final Color globalColor = Color.BLACK;
     // TODO find needed variables and methods
-    private GLabel label;
+    protected GLabel label;
+    protected GRect box;
+    protected GImage gImage;
+    protected boolean hovering;
+
+    public Button(String text) {
+        label = new GLabel(text);
+        label.setFont(globalFont);
+        label.setColor(globalColor);
+        box = new GRect(label.getWidth() + 10, label.getHeight() + 10);
+        box.setColor(globalColor);
+        add(box);
+        add(label);
+        label.setLocation(-label.getWidth() / 2, -label.getHeight() / 2);
+    }
 
     @Override
     public void onPress(MouseEvent e) {
