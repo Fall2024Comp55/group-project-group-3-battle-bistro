caeb3411713ffde9d021c7d75933f983d0791fe4
4Falcon4 <falcon@4falcon4.com>
2025-03-29 13:52:17 -0700

    refactor SpatulaProjectile to use GImage for visuals and improve image handling

--- src/Tower/SpatulaProjectile.java
+++ src/Tower/SpatulaProjectile.java
@@ -1,22 +1,19 @@
 package Tower;
 
+import Enemy.Enemy;
 import UI.GameScreen;
-import Utils.GameTick;
-import Utils.Utils;
 import acm.graphics.GImage;
 import acm.graphics.GPoint;
-import Enemy.Enemy;
-
-import javax.swing.*;
 
 public class SpatulaProjectile extends Projectile {
-    private GImage spatulaImage;
 
     public SpatulaProjectile(GPoint startPoint, GPoint targetPoint, Enemy targetEnemy, double speed, double moveRate, int damage) {
-        super(targetPoint, targetEnemy, speed, moveRate, damage);
+        super("spatula", targetPoint, targetEnemy, speed, moveRate, damage);
 
-      
-        this.setLocation(Utils.getCenterOffset(startPoint, 20, 20)); 
+        gImage = new GImage(getImage());
+
+        add(gImage);
+        gImage.setSize(20, 50);
 
      
         setupVisuals();
@@ -27,17 +24,6 @@ public class SpatulaProjectile extends Projectile {
 
     @Override
     protected void setupVisuals() {
-        // I don't know why when I add image it gives me some weird error so I'll use the placeholder
-        spatulaImage = new GImage("resources/spatula.png");
-        spatulaImage.setSize(20, 20);
-        add(spatulaImage);
 
-        // have to go this route
-        if (spatulaImage.getImage() == null) {
-            System.out.println("Spatula image not found, using placeholder.");
-            spatulaImage = new GImage(new ImageIcon(getClass().getResource("/resources/placeholder.png")).getImage());
-            spatulaImage.setSize(20, 20);
-            add(spatulaImage);
-        }
     }
 }
