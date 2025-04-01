fdb3ca9fdeaf4d59589581ccf025f208c4d99b1f
4Falcon4 <falcon@4falcon4.com>
2025-04-01 16:21:14 -0700

    feat: add interaction handling methods to Interact interface

--- src/Utils/Interact.java
+++ src/Utils/Interact.java
@@ -1,5 +1,9 @@
 package Utils;
 
+import UI.GameScreen;
+import acm.graphics.GObject;
+import acm.graphics.GRectangle;
+
 /**
  * The Interact interface should be implemented by any class that wants to handle
  * interaction events.
@@ -10,4 +14,27 @@ public interface Interact {
      * This method is called to perform an interaction.
      */
     void interact();
+
+    /**
+     * @return The GRectangle representing the hitbox.
+     */
+    GRectangle getInteractHitbox();
+
+    /**
+     * Checks if an interactable object's hitbox intersects with this object's hitbox.
+     *
+     * @return The Interact object that intersects with this object's hitbox, or null if none found.
+     */
+    default Interact checkForInteractable() {
+
+        for (GObject object : GameScreen.getInstance()) {
+            if (object instanceof Interact i && object != this) {
+                if (i.getInteractHitbox() != null && this.getInteractHitbox().intersects(i.getInteractHitbox())) {
+                    return i;
+                }
+            }
+        }
+        return null;
+    }
+
 }
