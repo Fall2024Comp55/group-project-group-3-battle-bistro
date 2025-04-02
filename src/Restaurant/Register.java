d93dec5db4fe18b45308c50d3114f2875c072093
Evan <e_phoukong@u.pacific.edu>
2025-04-02 16:49:03 -0700

    Register Implemented
    Register Object was implemented and capable of being added to Restaurant Screen.

--- src/Restaurant/Register.java
+++ src/Restaurant/Register.java
@@ -1,4 +1,61 @@
 package Restaurant;
 
-public class Register {
+import Food.Food;
+import Utils.Interact;
+import Utils.Solid;
+import acm.graphics.GCompound;
+import acm.graphics.GImage;
+import acm.graphics.GRectangle;
+
+import javax.swing.*;
+import java.awt.*;
+import java.net.URL;
+
+public class Register extends GCompound implements Solid, Interact {
+    private static final String basePath = "/resources/OrderWindow/";
+    private static final String extension = ".png";
+
+    private String name;
+    private GImage gImage;
+    protected Food item;
+
+    public Register(String name) {
+        this.name = name;
+        GImage gImage = new GImage(getImage());
+        this.gImage = gImage;
+        //gImage.setLocation(0, 0);
+        add(gImage);
+    }
+
+    public String toPath() {
+        return basePath + name.toLowerCase() + extension;
+    }
+
+    public Image getImage() {
+        URL resource = getClass().getResource(toPath());
+        if (resource != null) {
+            return new ImageIcon(resource).getImage();
+        }
+        throw new RuntimeException("Could not find image for path " + toPath());
+    }
+
+    @Override
+    public void interact() {
+        //TODO: Check if customer is present upon interaction, take order and retrieve order ticket.
+    }
+
+    @Override
+    public GRectangle getInteractHitbox() {
+        return this.getBounds();
+    }
+
+    @Override
+    public void onCollision() {
+
+    }
+
+    @Override
+    public GRectangle getHitbox() {
+        return null;
+    }
 }
