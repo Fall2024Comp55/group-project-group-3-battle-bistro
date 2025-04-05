bbcd10cfa962aed974cd7bee40f04673b1caa614
4Falcon4 <falcon@4falcon4.com>
2025-04-04 17:43:37 -0700

    feat: add background rectangle to OrderTicket and initialize elements set

--- src/Customer/OrderTicket.java
+++ src/Customer/OrderTicket.java
@@ -1,16 +1,26 @@
 package Customer;
 
 import Food.IngredientsType;
+import Utils.Utils;
 import acm.graphics.GCompound;
+import acm.graphics.GObject;
+import acm.graphics.GRect;
 
 import java.util.EnumSet;
+import java.util.Set;
 
 public class OrderTicket extends GCompound {
     private final EnumSet<IngredientsType> order;
+    private Set<GObject> elements;
+
 
     public OrderTicket() {
         order = EnumSet.noneOf(IngredientsType.class);
+        GRect background = new GRect(0, 0, 50, 80);
+        add(background);
+        background.setLocation(Utils.getCenter(background.getBounds()));
     }
+
 
     public EnumSet<IngredientsType> getOrder() {
         return order;
