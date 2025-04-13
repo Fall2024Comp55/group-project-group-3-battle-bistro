8e033436e8b0b2277484848f90d6c513b531e7b3
4Falcon4 <falcon@4falcon4.com>
2025-04-13 12:38:29 -0700

    refactor: update ActionButton and Button constructors for improved flexibility and centering

--- src/UI/NewTowerButton.java
+++ src/UI/NewTowerButton.java
@@ -11,8 +11,7 @@ import java.awt.event.MouseEvent;
 public class NewTowerButton extends Button {
     private Tower tower;
 
-    public NewTowerButton(String text) {
-        super(text);
+    public NewTowerButton() {
     }
 
     @Override
@@ -39,31 +38,7 @@ public class NewTowerButton extends Button {
     @Override
     public void onRelease(MouseEvent e) {
         if (tower != null) {
-            /*
-            GRectangle towerBounds = tower.getInteractHitbox();
-            boolean onPath = false;
-            for (GPoint point : GameScreen.getPath().getPoints()) {
-                GRectangle pointBounds = new GRectangle(point.getX(), point.getY(), 1, 1);
-                if (towerBounds.intersects(pointBounds)) {
-                    onPath = true;
-                    break;
-                }
-            }
-
-            if (onPath) {
-                Character.getInstance().addBalance(TOWER_COST);
-                GameScreen.getInstance().remove(tower);
-            } else {
-             
-                tower.onRelease(e);
-                GameTick.TickManager.registerTickListener(tower);
-            }
-            tower = null;
-            MouseManager.setSelectedObject(null);
-            */
-
             tower.onRelease(e);
-
         }
     }
 }
