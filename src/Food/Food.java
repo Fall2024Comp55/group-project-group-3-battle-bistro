8d2638a42bc8301bd92d3710c1635d213307bbcf
Evan <e_phoukong@u.pacific.edu>
2025-04-13 17:06:16 -0700

    FIXED: Layering of Pizza
    The pizza is layered properly so ingredients don't get covered by others.

--- src/Food/Food.java
+++ src/Food/Food.java
@@ -5,10 +5,14 @@ import Utils.Utils;
 import acm.graphics.GCompound;
 import acm.graphics.GImage;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.EnumSet;
+import java.util.HashMap;
 
 public class Food extends GCompound {
     private final EnumSet<IngredientsType> ingredients;
+    private HashMap<Integer, GImage> images;
     private OrderTicket orderTicket;
     private boolean sauce;
     private boolean cheese;
@@ -25,6 +29,7 @@ public class Food extends GCompound {
     public Food() {
         ingredients = EnumSet.noneOf(IngredientsType.class);
         image = new GCompound();
+        images = new HashMap<>();
         ingredients.add(IngredientsType.DOUGH);
         sauce = false;
         cheese = false;
@@ -43,12 +48,35 @@ public class Food extends GCompound {
 //        addIngredient(dough);
 //    }
 
-
     public void addIngredient(IngredientsType ingredient) {
         ingredients.add(ingredient);
         GImage ingred = new GImage(Utils.getImage(ingredient.toIngredientPath()));
         ingred.setSize(40, 40);
-        image.add(ingred);
+        switch (ingredient) {
+            case DOUGH:
+                images.put(1, ingred);
+                break;
+            case SAUCE:
+                images.put(2, ingred);
+                break;
+            case MOZZARELLA:
+                images.put(3, ingred);
+                break;
+            case PEPPERONI:
+                images.put(4, ingred);
+                break;
+            case MUSHROOM:
+                images.put(5, ingred);
+                break;
+        }
+        image.removeAll();
+        ArrayList<Integer> keys = new ArrayList<>(images.keySet());
+        Collections.sort(keys);
+        for (Integer key : keys) {
+            image.add(images.get(key));
+            System.out.println(key);
+            System.out.println("KEY ABOVE");
+        }
     }
 
     public boolean hasIngredient(IngredientsType ingredient) {
