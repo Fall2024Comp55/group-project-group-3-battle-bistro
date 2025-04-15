01f012173de5a8e4a73f6bb55395544cec6d4c40
4Falcon4 <falcon@4falcon4.com>
2025-04-15 10:36:01 -0700

    feat: enhance tower button functionality with cost label and improved positioning

--- src/UI/GardenUI.java
+++ src/UI/GardenUI.java
@@ -54,9 +54,10 @@ public class GardenUI extends UI implements Solid {
     public void initTowerPanel() {
         for (Towers tower : Towers.values()) {
             tower.createTower();
-            Button towerButton = new NewTowerButton(tower);
+            NewTowerButton towerButton = new NewTowerButton(tower);
             add(towerButton);
-            towerButton.setLocation(20 + (towerButton.getWidth() + 10) * (tower.ordinal() + 1), 5);
+            towerButton.setLocation(healthLabel.getX() + healthLabel.getWidth() + 20 + (towerButton.getWidth() + 10) * (tower.ordinal() + 1), this.getHeight() / 2 - towerButton.getHeight() / 2);
+            towerButton.addCostLabel();
         }
     }
 
--- src/UI/NewTowerButton.java
+++ src/UI/NewTowerButton.java
@@ -5,15 +5,28 @@ import Screen.GardenScreen;
 import Tower.Tower;
 import Tower.Towers;
 import Utils.MouseManager;
+import acm.graphics.GLabel;
 
 import java.awt.event.MouseEvent;
 
+import static Screen.ProgramWindow.GLOBAL_COLOR;
+
 public class NewTowerButton extends Button {
     private Towers tower;
+    private GLabel cost;
 
     public NewTowerButton(Towers tower) {
-        super(tower, 20);
+        super(tower, 25);
         this.tower = tower;
+        cost = new GLabel(String.valueOf(tower.getCost()) + "$");
+        cost.setFont("Arial-12");
+        cost.setColor(GLOBAL_COLOR);
+
+        cost.setLocation(cost.getWidth() / 2 - cost.getWidth(), getHeight());
+    }
+
+    public void addCostLabel() {
+        add(cost);
     }
 
 
