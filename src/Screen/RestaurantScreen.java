419e66c2733f9fd4d8ec1dfca004307739373eb4
4falcon4 <falcon@4falcon4.com>
2025-04-17 21:54:34 -0700

    refactor: clean up RestaurantScreen by removing commented-out code and improving method structure

--- src/Screen/RestaurantScreen.java
+++ src/Screen/RestaurantScreen.java
@@ -23,7 +23,6 @@ public class RestaurantScreen extends Screen {
 
     private static CustomerPath customerPath;
 
-
     private GImage background;
     private ScheduledExecutorService dayTimer; // Timer for day end
 
@@ -47,7 +46,6 @@ public class RestaurantScreen extends Screen {
 
     @Override
     public void initializeComponents() {
-
         GImage wall_image  = new GImage(Utils.getImage("/resources/restaurant/wall.png"));
         wall_image.setLocation(0, 0);
         wall_image.setSize(800, 100);
@@ -116,8 +114,6 @@ public class RestaurantScreen extends Screen {
         prepTable4.sendToFront();
         add(prepTable4);
 
-
-
         // Oven for cooking pizzas
         Oven oven = new Oven();
         oven.setLocation(200, 20);
@@ -131,12 +127,10 @@ public class RestaurantScreen extends Screen {
         prep_table_image.sendToBack();
         prep_table_image.sendForward();
 
-
 //        // Order window for delivering pizzas
 //        OrderWindow orderWindow = new OrderWindow("Order Window");
 //        orderWindow.setLocation(450, 200);
 //        add(orderWindow);
-
 
         customerPath = new CustomerPath(600, 300, 500, 300, 500, 100, 600, 100);
         customerPath.addPath(this);
@@ -148,32 +142,24 @@ public class RestaurantScreen extends Screen {
         background = new GImage(Utils.getImage(FLOOR_PATH));
         add(background);
         background.sendToBack();
-        
-        
-     // Start 20-second timer for day end
-     // dayTimer = Executors.newSingleThreadScheduledExecutor();
-     // dayTimer.schedule(this::endDay, 20, TimeUnit.SECONDS);
-        
-
-
 
 //        add(OrderTicketUI.getInstance());
 //        elements.add(OrderTicketUI.getInstance());
         
-        
-        
-        
     }
-//    private void endDay() {
-//        // Stop the timer
-//        dayTimer.shutdown();
-//        // Transition to SummaryMenuScreen
-//        ProgramWindow.getInstance().endDay();
-//    }
-    
-    
-    
-    
+
+    public void resetDayTick() {
+        dayTick = 0;
+    }
+
+    public void incrementDayTick() {
+        dayTick++;
+        if (dayTick >= dayLength) {
+            dayTick = 0;
+            ProgramWindow.getInstance().endDay();
+        }
+    }
+
     
     @Override
     public synchronized void registerTickListener(TickListener listener) {
@@ -195,125 +181,6 @@ public class RestaurantScreen extends Screen {
         screenExecutor.submit(() -> {
             restaurantTickListeners.forEach(TickListener::onTick);
         });
+        incrementDayTick();
     }
 }
-
-
-
-
-//package Screen;
-//import Character.Character;
-//import Customer.Customer;
-//import Food.IngredientsType;
-//import Restaurant.*;
-//import UI.OrderTicketUI;
-//import UI.RestaurantUI;
-//import Utils.TickListener;
-//
-//import java.util.Set;
-//
-//public class RestaurantScreen extends Screen {
-//    private volatile Set<TickListener> restaurantTickListeners;
-//
-//    private static final RestaurantScreen RESTAURANT_SCREEN;
-//
-//    static {
-//        try {
-//            RESTAURANT_SCREEN = new RestaurantScreen();
-//        } catch (Exception e) {
-//            throw new RuntimeException("Exception occurred in creating RestaurantScreen singleton instance");
-//        }
-//    }
-//
-//    private RestaurantScreen() {
-//        // Initialize the restaurant screen components here
-//        add(Character.getInstance());
-//        initializeComponents();
-//    }
-//
-//    public static RestaurantScreen getInstance() {
-//        return RESTAURANT_SCREEN;
-//    }
-//
-//    @Override
-//    public void initializeComponents() {
-//        add(RestaurantUI.getInstance());
-//        add(OrderTicketUI.getInstance());
-//
-//        // door to switch back to the Garden screen
-//        Door door = new Door();
-//        door.setLocation(0, 50);
-//        elements.add(door);
-//        add(door);
-//
-//        // ingredient stations
-//        IngredientStation doughStation = new IngredientStation(IngredientsType.DOUGH);
-//        doughStation.setLocation(100, 300);
-//        elements.add(doughStation);
-//        add(doughStation);
-//
-//        /*
-//        IngredientStation pepperoniStation = new IngredientStation(IngredientsType.PEPPERONI);
-//        pepperoniStation.setLocation(200, 300);
-//        elements.add(pepperoniStation);
-//        add(pepperoniStation);
-//
-//        IngredientStation mozzarellaStation = new IngredientStation(IngredientsType.MOZZARELLA);
-//        mozzarellaStation.setLocation(300, 300);
-//        elements.add(mozzarellaStation);
-//        add(mozzarellaStation);
-//
-//        IngredientStation mushroomStation = new IngredientStation(IngredientsType.MUSHROOM);
-//        mushroomStation.setLocation(400, 300);
-//        elements.add(mushroomStation);
-//        add(mushroomStation);
-//        */
-//
-//        // prep table for assembling pizzas
-//        PrepTable prepTable = new PrepTable("Prep Table");
-//        prepTable.setLocation(250, 200);
-//        elements.add(prepTable);
-//        add(prepTable);
-//
-//        // oven for cooking pizzas
-//        Oven oven = new Oven();
-//        oven.setLocation(350, 200);
-//        elements.add(oven);
-//        add(oven);
-//
-//        // order window for delivering pizzas
-//        OrderWindow orderWindow = new OrderWindow("Order Window");
-//        orderWindow.setLocation(450, 200);
-//        elements.add(orderWindow);
-//        add(orderWindow);
-//
-//        //customer
-//        Customer customer = new Customer();
-//        customer.setLocation(500, 100);
-//        elements.add(customer);
-//        add(customer);
-//
-//    }
-//
-//    @Override
-//    public void registerTickListener(TickListener listener) {
-//
-//    }
-//
-//    @Override
-//    public void unregisterTickListener(TickListener listener) {
-//
-//    }
-//
-//    @Override
-//    public void unregisterAllTickListener() {
-//
-//    }
-//
-//    @Override
-//    public void onTick() {
-//        screenExecutor.submit(() -> {
-//            restaurantTickListeners.spliterator().forEachRemaining(TickListener::onTick);
-//        });
-//    }
-//}
