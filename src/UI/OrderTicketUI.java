d16376848b18a892f34d8aae9e3ff40fd496b041
4Falcon4 <falcon@4falcon4.com>
2025-04-22 13:53:53 -0700

    Refactor OrderTicketUI class to remove unused code and constants

--- src/UI/OrderTicketUI.java
+++ src/UI/OrderTicketUI.java
@@ -12,8 +12,6 @@ public class OrderTicketUI extends UI {
     private static final OrderTicketUI ORDER_TICKET_UI;
     public static final int HORIZONTAL_OFFSET = 30; //50
     public static final int VERTICAL_OFFSET = 50; //150
-    public static final int POS_X = 736;
-    public static final int POS_Y = 50;
     private boolean hidden = true;
 
     private static final ArrayList<OrderTicket> tickets;
@@ -81,59 +79,3 @@ public class OrderTicketUI extends UI {
         return tickets;
     }
 }
-
-
-
-//package UI;
-//
-//import Customer.Customer;
-//import Customer.OrderTicket;
-//
-//import java.util.ArrayList;
-//
-//public class OrderTicketUI extends UI {
-//
-//    private static final OrderTicketUI ORDER_TICKET_UI;
-//
-//    protected ArrayList<OrderTicket> tickets;
-//    public static final int horizontalOffset = 50;
-//    public static final  int verticalOffset = 150;
-//
-//    static {
-//        try {
-//            ORDER_TICKET_UI = new OrderTicketUI();
-//        } catch (Exception e) {
-//            throw new RuntimeException("Exception occurred in creating OrderTicketUI singleton instance");
-//        }
-//    }
-//
-//    public static OrderTicketUI getInstance() {
-//        return ORDER_TICKET_UI;
-//    }
-//
-//    public OrderTicketUI() {
-//        tickets = new ArrayList<>();
-//        Customer c1 = new Customer();
-//        Customer c2 = new Customer();
-//        OrderTicket ticket1 = c1.getOrderTicket();
-//        OrderTicket ticket2 = c2.getOrderTicket();
-//        tickets.add(ticket1);
-//        tickets.add(ticket2);
-//        int count = 1;
-//        for (OrderTicket ticket : tickets) {
-//            ticket.setLocation(horizontalOffset, verticalOffset * count);
-//            add(ticket);
-//            count++;
-//        }
-//    }
-//
-//    public void addTicket(OrderTicket ticket) {
-//        tickets.add(ticket);
-//        ticket.setLocation(horizontalOffset, verticalOffset * tickets.size());
-//        add(ticket);
-//    }
-//
-//    public ArrayList<OrderTicket> getTickets() {
-//        return tickets;
-//    }
-//}
