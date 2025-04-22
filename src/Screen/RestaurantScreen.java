bfa74f44d17a3d10fb0dd93de7451f1f97c89b61
4Falcon4 <falcon@4falcon4.com>
2025-04-22 10:24:45 -0700

    Enhance RestaurantScreen layout. Converted paths to const and component organization

--- src/Screen/RestaurantScreen.java
+++ src/Screen/RestaurantScreen.java
@@ -16,12 +16,13 @@ import java.util.Set;
 public class RestaurantScreen extends Screen {
     private volatile Set<TickListener> restaurantTickListeners;
     private static final RestaurantScreen RESTAURANT_SCREEN;
+    public static final String PREP_TABLE_PATH = "/resources/restaurant/prep_table.png";
+    public static final String WALL_PATH = "/resources/restaurant/wall.png";
     private static final String FLOOR_PATH = "/resources/restaurant/floor.jpg";
     private static final int dayLength = 1200000; // in ticks
 
     private static CustomerPath customerPath;
 
-    private GImage background;
     private static long dayTick;
     private static boolean dayStarted;
 
@@ -45,106 +46,107 @@ public class RestaurantScreen extends Screen {
 
     @Override
     public void initializeComponents() {
-        GImage wall_image = new GImage(Utils.getImage("/resources/restaurant/wall.png"));
+        // Background image
+        GImage wall_image = new GImage(Utils.getImage(WALL_PATH));
         wall_image.setLocation(0, 0);
         wall_image.setSize(800, 100);
         add(wall_image);
 
+        GImage background = new GImage(Utils.getImage(FLOOR_PATH));
+        add(background);
+        background.sendToBack();
+
+        // Long prep table image
+        GImage prep_table_image = new GImage(Utils.getImage(PREP_TABLE_PATH));
+        prep_table_image.setLocation(0, 450);
+        prep_table_image.setSize(250, 50);
+        prep_table_image.rotate(90);
+        add(prep_table_image);
+
+        // Door for entering and exiting the restaurant
         Door door = new Door();
         door.setLocation(0, 50);
         add(door);
 
+        // Trash can for disposing of items
         TrashCan trashCan = new TrashCan();
         trashCan.setLocation(50, 50);
         add(trashCan);
 
+        // Order window for taking orders
         OrderWindow orderWindow = new OrderWindow();
         orderWindow.setLocation(450, 50);
         add(orderWindow);
-
-        IngredientStation doughStation = new IngredientStation(IngredientsType.DOUGH);
-        doughStation.setLocation(225, 200);
-        add(doughStation);
-
-        IngredientStation pepperoniStation = new IngredientStation(IngredientsType.PEPPERONI);
-        pepperoniStation.setLocation(315, 275);
-        add(pepperoniStation);
-
-        IngredientStation mozzarellaStation = new IngredientStation(IngredientsType.MOZZARELLA);
-        mozzarellaStation.setLocation(135, 275);
-        add(mozzarellaStation);
-
-        IngredientStation mushroomStation = new IngredientStation(IngredientsType.MUSHROOM);
-        mushroomStation.setLocation(285, 200);
-        add(mushroomStation);
-
-        IngredientStation sauceStation = new IngredientStation(IngredientsType.SAUCE);
-        sauceStation.setLocation(165, 200);
-        add(sauceStation);
-
-        PrepTable prepTable5 = new PrepTable();
-        prepTable5.setLocation(0, 250);
-        prepTable5.rotate(90);
-        prepTable5.sendToFront();
-        add(prepTable5);
-
-        // Prep table for assembling pizzas
-        PrepTable prepTable1 = new PrepTable();
-        prepTable1.setLocation(0, 300);
-        prepTable1.rotate(90);
-        prepTable1.sendToFront();
-        add(prepTable1);
-
-        PrepTable prepTable2 = new PrepTable();
-        prepTable2.setLocation(0, 350);
-        prepTable2.rotate(90);
-        prepTable2.sendToFront();
-        add(prepTable2);
-
-        PrepTable prepTable3 = new PrepTable();
-        prepTable3.setLocation(0, 400);
-        prepTable3.rotate(90);
-        prepTable3.sendToFront();
-        add(prepTable3);
-
-        PrepTable prepTable4 = new PrepTable();
-        prepTable4.setLocation(0, 450);
-        prepTable4.rotate(90);
-        prepTable4.sendToFront();
-        add(prepTable4);
 
         // Oven for cooking pizzas
         Oven oven = new Oven();
         oven.setLocation(200, 20);
         add(oven);
 
-        GImage prep_table_image = new GImage(Utils.getImage("/resources/restaurant/prep_table.png"));
-        prep_table_image.setLocation(0, 450);
-        prep_table_image.setSize(250, 50);
-        prep_table_image.rotate(90);
-        add(prep_table_image);
-        prep_table_image.sendToBack();
-        prep_table_image.sendForward();
+        // Ingredient stations for pizza making
+        {
+            IngredientStation doughStation = new IngredientStation(IngredientsType.DOUGH);
+            doughStation.setLocation(225, 200);
+            add(doughStation);
 
-//        // Order window for delivering pizzas
-//        OrderWindow orderWindow = new OrderWindow("Order Window");
-//        orderWindow.setLocation(450, 200);
-//        add(orderWindow);
+            IngredientStation pepperoniStation = new IngredientStation(IngredientsType.PEPPERONI);
+            pepperoniStation.setLocation(315, 275);
+            add(pepperoniStation);
+
+            IngredientStation mozzarellaStation = new IngredientStation(IngredientsType.MOZZARELLA);
+            mozzarellaStation.setLocation(135, 275);
+            add(mozzarellaStation);
+
+            IngredientStation mushroomStation = new IngredientStation(IngredientsType.MUSHROOM);
+            mushroomStation.setLocation(285, 200);
+            add(mushroomStation);
+
+            IngredientStation sauceStation = new IngredientStation(IngredientsType.SAUCE);
+            sauceStation.setLocation(165, 200);
+            add(sauceStation);
+        }
+
+        // Prep table for preparing ingredients
+        {
+            PrepTable prepTable0 = new PrepTable();
+            prepTable0.setLocation(0, 250);
+            prepTable0.rotate(90);
+            prepTable0.sendToFront();
+            add(prepTable0);
+
+            // Prep table for assembling pizzas
+            PrepTable prepTable1 = new PrepTable();
+            prepTable1.setLocation(0, 300);
+            prepTable1.rotate(90);
+            prepTable1.sendToFront();
+            add(prepTable1);
+
+            PrepTable prepTable2 = new PrepTable();
+            prepTable2.setLocation(0, 350);
+            prepTable2.rotate(90);
+            prepTable2.sendToFront();
+            add(prepTable2);
+
+            PrepTable prepTable3 = new PrepTable();
+            prepTable3.setLocation(0, 400);
+            prepTable3.rotate(90);
+            prepTable3.sendToFront();
+            add(prepTable3);
+
+            PrepTable prepTable4 = new PrepTable();
+            prepTable4.setLocation(0, 450);
+            prepTable4.rotate(90);
+            prepTable4.sendToFront();
+            add(prepTable4);
+        }
 
         customerPath = new CustomerPath(600, 300, 500, 300, 500, 100, 600, 100);
         customerPath.addPath(this);
         Customer.setPath(customerPath);
 
+        // Add a test customer
         Customer testCustomer = new Customer();
         add(testCustomer);
-
-        background = new GImage(Utils.getImage(FLOOR_PATH));
-        add(background);
-        background.sendToBack();
-
-//        add(OrderTicketUI.getInstance());
-//        elements.add(OrderTicketUI.getInstance());
-
     }
 
     public static void resetDay() {
