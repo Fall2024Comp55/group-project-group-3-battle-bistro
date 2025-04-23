8f06b025b33c58ac083bd5d57f535f08238b1adc
Evan <e_phoukong@u.pacific.edu>
2025-04-23 16:35:05 -0700

    Box Station
    Boxing station is now implemented

---
+++ src/Restaurant/Box.java
@@ -0,0 +1,47 @@
+package Restaurant;
+
+import Character.Character;
+import Food.Food;
+import Utils.Interact;
+import Utils.Solid;
+import Utils.Utils;
+import acm.graphics.GCompound;
+import acm.graphics.GImage;
+import acm.graphics.GRectangle;
+
+public class Box extends GCompound implements Solid, Interact {
+    private static final String PATH = "/resources/restaurant/Pizza_Box.png";
+
+    private final GImage gImage;
+
+    public Box() {
+        GImage gImage = new GImage(Utils.getImage(PATH));
+        this.gImage = gImage;
+        gImage.setSize(45, 64);
+        add(gImage);
+    }
+
+    @Override
+    public void interact() {
+        System.out.println("Boxing");
+        Food pizza = Character.getInstance().getHolding();
+        if (pizza != null && pizza.isCooked()) {
+            pizza.setBoxed(true);
+        }
+    }
+
+    @Override
+    public GRectangle getInteractHitbox() {
+        return this.getBounds();
+    }
+
+    @Override
+    public void onCollision() {
+
+    }
+
+    @Override
+    public GRectangle getHitbox() {
+        return null;
+    }
+}
